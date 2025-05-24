790d7bac48fe3d3c4bcabde4f778e18772345358
keirinjingle <keirinjingle@gmail.com>
2025-05-25 05:41:16 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
+const CACHE_NAME = "keirin-cache-v1.5";
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('keirin-cache').then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         './',
         './index.html',
-        './main.js',
+        './main.js?v=1.5',
         './manifest.json',
         './icon.png'
       ]);
@@ -15,5 +17,15 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
